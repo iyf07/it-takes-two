69060be7ba74b976b83cf1b4b079d5be16932339
Ivan Yang <ivan@ivans-mbp.lan>
2022-11-26 01:22:53 -0500

    Change port

--- app.js
+++ app.js
@@ -53,6 +53,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).send(message)
 })
 
-app.listen(3000, () => {
-    console.log('Serving on port 3000')
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
 })
