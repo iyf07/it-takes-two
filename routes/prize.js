59a56082f47675a70aaef5d9f15d36bebf2a3c2e
Ivan Yang <ivan@Ivans-MacBook-Pro.local>
2022-12-26 11:40:35 -0500

    Reformat

--- routes/prize.js
+++ routes/prize.js
@@ -5,40 +5,42 @@ const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const PiggyModel = require("../models/piggy-model");
-const themeColorInventory = '#7B542C';
-const themeColorStore = '#fa554f';
 
 router.get('/store', catchAsync(async (req, res) => {
     const prizes = await Store.find({}).sort({priority: 1});
     const currency = await Currency.find({});
     const user = req.cookies;
-    res.render('prize/store', {prizes, currency, themeColorStore, user});
+    const themeColor = '#fa554f';
+    res.render('prize/store', {prizes, currency, themeColor, user});
 }))
 
 router.get('/store/new-prize', async (req, res) => {
     const currency = await Currency.find({});
     const user = req.cookies;
-    res.render('prize/store/new', {currency, themeColorStore, user});
+    const themeColor = '#fa554f';
+    res.render('prize/store/new', {currency, themeColor, user});
 })
 
 router.get('/store/:id/edit', catchAsync(async (req, res) => {
     const prize = await PiggyModel.findById(req.params.id);
     const currency = await Currency.find({});
     const user = req.cookies;
-    res.render('prize/store/edit', {prize, currency, themeColorStore, user});
+    const themeColor = '#fa554f';
+    res.render('prize/store/edit', {prize, currency, themeColor, user});
 }));
 
 router.get('/inventory', catchAsync(async (req, res) => {
-    const inventories = await Inventory.find({}).sort({priority: 1, date: -1});
+    const prizes = await Inventory.find({}).sort({priority: 1, date: -1});
     const currency = await Currency.find({});
     const user = req.cookies;
-    res.render('prize/inventory', {inventories, currency, themeColorInventory, user});
+    const themeColor = '#7B542C';
+    res.render('prize/inventory', {prizes, currency, themeColor, user});
 }))
 
 router.put('/inventory/sent/:id', catchAsync(async (req, res) => {
     const id = req.params.id;
-    const curinventory = await Inventory.findById(id);
-    if (curinventory.main) {
+    const curInventory = await Inventory.findById(id);
+    if (curInventory.main) {
         await Inventory.findByIdAndUpdate(id, {main: false, secondary: false, priority: 2});
     } else {
         await Inventory.findByIdAndUpdate(id, {main: true, secondary: false, priority: 1});
@@ -48,11 +50,11 @@ router.put('/inventory/sent/:id', catchAsync(async (req, res) => {
 
 router.put('/inventory/received/:id', catchAsync(async (req, res) => {
     const id = req.params.id;
-    const curinventory = await Inventory.findById(id);
-    if (curinventory.secondary) {
+    const curInventory = await Inventory.findById(id);
+    if (curInventory.secondary) {
         await Inventory.findByIdAndUpdate(id, {main: false, secondary: false, priority: 2});
     } else {
-        await Inventory.findByIdAndUpdate(id, {main: true, secondary: true, priority: 0, receivedate: new Date()});
+        await Inventory.findByIdAndUpdate(id, {main: true, secondary: true, priority: 0, receivedDate: new Date()});
     }
     res.redirect(`/prize/inventory`);
 }))
